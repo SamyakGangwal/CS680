8ab01b4d464de8c32681f1d421161a515bfc0a61
Samyak Gangwal <sam.gangwal97@gmail.com>
2022-12-16 19:46:44 -0500

    Updated Homework assignment 17

--- test/src/edu/umb/cs680/hw17/StockObservableQuoteTest.java
+++ test/src/edu/umb/cs680/hw17/StockObservableQuoteTest.java
@@ -1,6 +1,7 @@
 package edu.umb.cs680.hw17;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 public class StockObservableQuoteTest {
@@ -9,35 +10,40 @@ public class StockObservableQuoteTest {
 	public void notifyStockQuoteObservers() {
 		LineChartObserver lineChartObserver = new LineChartObserver();
 		T3DObserver t3dObserver = new T3DObserver();
-		TableObserver tableObserver = new TableObserver();
+		
+		Observer updateTableObserver = (Observable sender, Object event) -> {System.out.println("Stock: " + ((StockEvent)event).getTicker());};
 		
 
 		StockQuoteObservable st = new StockQuoteObservable();
 
-		st.addObserver(tableObserver);
+		st.addObserver(updateTableObserver);
 		st.addObserver(t3dObserver);
 		st.addObserver(lineChartObserver);
 
 		st.changeQuote("AAPL", 568.6);
 
-		assertEquals("AAPL", tableObserver.getS().getTicker());
-		assertEquals("AAPL", t3dObserver.getS().getTicker());
 		assertEquals("AAPL", lineChartObserver.getS().getTicker());
+		assertEquals("AAPL", t3dObserver.getS().getTicker());
+		
 
-		assertEquals(568.6, tableObserver.getS().getQuote());
 		assertEquals(568.6, t3dObserver.getS().getQuote());
 		assertEquals(568.6, lineChartObserver.getS().getQuote());
 
 
 		st.changeQuote("GOOGL", 1456.6);
 
-		assertEquals("GOOGL", tableObserver.getS().getTicker());
+		
 		assertEquals("GOOGL", t3dObserver.getS().getTicker());
 		assertEquals("GOOGL", lineChartObserver.getS().getTicker());
 
-		assertEquals(1456.6, tableObserver.getS().getQuote());
 		assertEquals(1456.6, t3dObserver.getS().getQuote());
 		assertEquals(1456.6, lineChartObserver.getS().getQuote());
+
+		st.removeObserver(updateTableObserver);
+		st.removeObserver(lineChartObserver);
+		st.removeObserver(t3dObserver);
+
+		assertTrue(st.getObservers().isEmpty());
 
 	}
 }
